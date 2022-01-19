d0026e2cf538452795ee3e9335fa77d03b4d73b9
Karmel Salah <karmel@morphysm.com>
2022-01-19 13:04:35 +0200

    push files

---
+++ src/components/Body.jsx
@@ -0,0 +1,312 @@
+import React from "react";
+import "./Body.css";
+const Body = () => {
+  return (
+    <div id="page" class="site">
+      <header id="masthead" class="site-header">
+        <div class="site-branding">
+          <p class="site-title">
+            <a href="https://princecafe861690252.wordpress.com/" rel="home">
+              Prince Cafe
+            </a>
+          </p>
+        </div>
+        <nav
+          id="site-navigation"
+          class="main-navigation"
+          aria-label="Main Navigation"
+        >
+          <input
+            type="checkbox"
+            role="button"
+            aria-haspopup="true"
+            id="toggle"
+            class="hide-visually"
+          />
+          <label id="toggle-menu" class="button">
+            Menu <span class="hide-visually expanded-text">expanded</span>
+            <span class="hide-visually collapsed-text">collapsed</span>
+          </label>
+          <div class="menu-primary-container">
+            <ul id="menu-primary" class="main-menu" aria-label="submenu">
+              <li
+                id="menu-item-3"
+                class="menu-item menu-item-type-custom menu-item-object-custom menu-item-3"
+              >
+                <a href="/">Home</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+
+      <div id="content" class="site-content">
+        <section id="primary" class="content-area">
+          <main id="main" class="site-main">
+            <article
+              id="post-2"
+              class="post-2 page type-page status-publish hentry entry"
+            >
+              <div class="entry-content">
+                <div
+                  class="wp-block-cover alignfull is-light has-parallax"
+                  className="content"
+                >
+                  <span
+                    aria-hidden="true"
+                    class="has-background-dim-20 wp-block-cover__gradient-background has-background-dim"
+                    className="content2"
+                  ></span>
+                  <div class="wp-block-cover__inner-container">
+                    <hr class="wp-block-separator" />
+
+                    <div
+                      class="wp-block-spacer"
+                      className="content3"
+                      aria-hidden="true"
+                    ></div>
+
+                    <h1
+                      class="has-text-align-center"
+                      id="way-too-much-coffee"
+                      className="text"
+                    >
+                      <em>Way too much coffee</em>
+                    </h1>
+
+                    <div
+                      className="content3"
+                      aria-hidden="true"
+                      class="wp-block-spacer"
+                    ></div>
+
+                    <hr class="wp-block-separator" />
+                  </div>
+                </div>
+              </div>
+
+              <div
+                className="content4"
+                aria-hidden="true"
+                class="wp-block-spacer"
+              ></div>
+
+              <h3 class="has-text-align-center" id="welcome-to-rockfield">
+                <em>Welcome to Prince Cafe</em>
+              </h3>
+
+              <div
+                className="content3"
+                aria-hidden="true"
+                class="wp-block-spacer"
+              ></div>
+
+              <p>
+                Chef Patron Joan Smith and Executive Chef François Lemoine have
+                teamed up to open one of the most exciting restaurants in
+                Anytown.
+              </p>
+
+              <p>
+                Our specialty is delicious&nbsp;cuisine created with the
+                ingenuity and passion inspired by the fantastic flavors of
+                France&nbsp;itself. We take great pride in improving upon your
+                favorite French&nbsp;dishes in new and creative ways.
+              </p>
+
+              <div
+                className="content4"
+                aria-hidden="true"
+                class="wp-block-spacer"
+              ></div>
+
+              <div
+                class="wp-block-cover alignfull is-light has-parallax"
+                className="content7"
+              >
+                <span
+                  aria-hidden="true"
+                  class="has-background-dim-20 wp-block-cover__gradient-background has-background-dim"
+                  className="content6"
+                ></span>
+                <div class="wp-block-cover__inner-container">
+                  <hr class="wp-block-separator" />
+
+                  <div
+                    className="content3"
+                    aria-hidden="true"
+                    class="wp-block-spacer"
+                  ></div>
+
+                  <h2
+                    class="has-text-align-center"
+                    id="discover-our-menu"
+                    className="text"
+                  >
+                    <em>Discover Our Menu</em>
+                  </h2>
+
+                  <div
+                    className="content3"
+                    aria-hidden="true"
+                    class="wp-block-spacer"
+                  ></div>
+
+                  <hr class="wp-block-separator" />
+                </div>
+              </div>
+
+              <div
+                className="content4"
+                aria-hidden="true"
+                class="wp-block-spacer"
+              ></div>
+
+              <p>
+                The menu is a mix of French&nbsp;regional cuisines, and the menu
+                changes with the seasons.
+                <br />
+                Be sure to ask about the daily specials!
+              </p>
+
+              <div
+                className="content5"
+                aria-hidden="true"
+                class="wp-block-spacer"
+              ></div>
+
+              <hr class="wp-block-separator" />
+
+              <div
+                className="content5"
+                aria-hidden="true"
+                class="wp-block-spacer"
+              ></div>
+
+              <h3 class="has-text-align-center" id="sample-menu">
+                <em>Sample Menu</em>
+              </h3>
+
+              <div
+                className="content3"
+                aria-hidden="true"
+                class="wp-block-spacer"
+              ></div>
+
+              <div class="wp-block-columns alignwide">
+                <div class="wp-block-column">
+                  <h6 id="starter">Starter</h6>
+
+                  <p>
+                    <strong>Fricassée of Wild Mushrooms</strong>
+                    <br />
+                    <em>
+                      Braised wild mushrooms served with seaweed and liquid
+                      nitrogen
+                    </em>
+                    <br />
+                    $14.50
+                  </p>
+                </div>
+
+                <div class="wp-block-column">
+                  <h6 id="main">Main</h6>
+
+                  <p>
+                    <strong>
+                      Coq au Vin d&#8217;Alsace
+                      <br />
+                    </strong>
+                    <em>
+                      Chicken cooked in Riesling with onions, mushrooms and
+                      herbs
+                    </em>
+                    <br />
+                    $25.50
+                  </p>
+                </div>
+
+                <div class="wp-block-column">
+                  <h6 id="dessert">Dessert</h6>
+
+                  <p>
+                    <strong>Pumpkin Soufflé</strong>
+                    <br />
+                    <em>With bourbon, cinnamon and molasses sauce</em>
+                    <br />
+                    $9.00
+                  </p>
+                </div>
+              </div>
+
+              <div
+                className="content3"
+                aria-hidden="true"
+                class="wp-block-spacer"
+              ></div>
+
+              <div class="wp-block-button aligncenter">
+                <a
+                  class="wp-block-button__link"
+                  href="https://rockfielddemo.wordpress.com/menu/"
+                >
+                  View Full Menu
+                </a>
+              </div>
+
+              <div
+                className="content4"
+                aria-hidden="true"
+                class="wp-block-spacer"
+              ></div>
+
+              <div
+                class="wp-block-cover alignfull is-light has-parallax"
+                className="content8"
+              >
+                <span
+                  aria-hidden="true"
+                  class="has-background-dim-20 wp-block-cover__gradient-background has-background-dim"
+                  className="content6"
+                ></span>
+                <div class="wp-block-cover__inner-container">
+                  <hr class="wp-block-separator" />
+
+                  <div
+                    className="content3"
+                    aria-hidden="true"
+                    class="wp-block-spacer"
+                  ></div>
+
+                  <h2 class="has-text-align-center" id="book-your-table">
+                    <em>Book Your Table</em>
+                  </h2>
+
+                  <div
+                    className="content3"
+                    aria-hidden="true"
+                    class="wp-block-spacer"
+                  ></div>
+
+                  <hr class="wp-block-separator" />
+                </div>
+              </div>
+
+              <div
+                className="content4"
+                aria-hidden="true"
+                class="wp-block-spacer"
+              ></div>
+
+              <p>
+                To book a table, call us (123) 444–5678 or use the form below.
+              </p>
+            </article>
+          </main>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default Body;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const websocketURL = "ws://localhost:8082";
+
+const audioEnabled = true;
+const userIcon = require("./assets/person.png");
+const closeIcon = require("./assets/close.png");
+const sendIcon = require("./assets/send.png");
+const widgetIcon = require("./assets/cafe.png");
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      websocketURL={websocketURL}
+      audioEnabled={audioEnabled}
+      userIcon={userIcon}
+      closeIcon={closeIcon}
+      sendIcon={sendIcon}
+      widgetIcon={widgetIcon}
+    />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
